31fadc08700556259c5e324606b5da6383411895
bem-amado <bemamadoledev@yahoo.com>
2022-11-29 23:20:55 +0100

    95% of check module finished

--- src/check.c
+++ src/check.c
@@ -8,33 +8,72 @@
 int
 isReg(char *reg) {
 
-    if(*reg =='$'){
-        //printf("is a reg\n");
-        for(int i=0; i<=31; i++){
-            if(strcmp(*reg,regs[i].s))
+    if(reg[0] =='$'){
+        for(int i=0; i<31; i++){
+            if(strcmp(reg,regs[i].s) == 0){
                 return 1;
-            else
-                return 0;
-
+            }
         }
 
-    }else {
-       return 0;
     }
-
-    return 1;
-
+    return 0;
 }
 
 int
 isLabel(char *label){
 
-    if(isalpha(*label) !=0){
+    int n = strlen(label);
 
+    if (isdigit(label[0]) != 0){
+    }else{
+        if(label[n-1]==':'){
+            return 1;
+        }
     }
+    return 0;
 }
 
+int
+isoperator(const char *operator){
+    //to uppercase operator
 
+    if(isdigit(operator[0]) || strlen(operator) > 4){
+    }else {
+        for(int i=0; i<25; i++){
+            if(strcmp(operator,oplst[i].name) == 0){
+                return 1;
+            }
+        }
+    }
+ return 0;
+}
+
+int
+isImm_5(char *imm){
+    int n = atoi(imm);
+
+    if(isdigit(*imm)!=0){
+        if ((n & 31) == n){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int
+isImm_16(char *imm){
+    int n = atoi(imm);
+
+    if(isdigit(n)!= 0){
+        printf("is a digit\n");
+        if ((n & 65535) == n){
+            printf("yes\n");
+            return 1;
+        }
+    }
+    printf("aii\n");
+    return 0;
+}
 
 
 
--- src/check.h
+++ src/check.h
@@ -5,5 +5,6 @@
 int isReg(char *reg);
 int isLabel(char *label);
 int isImm_5(char *imm);
-int isImm_16(char *s);
+int isImm_16(char *imm);
+int isoperator(const char *operator);
 void checkproto(void);
